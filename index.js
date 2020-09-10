3878bd3e363e4bb12c4e2169beeaa09f190c0bf6
ErrojuSaikumar <48911415+ErrojuSaikumar@users.noreply.github.com>
2020-09-10 21:33:38 +0530

    commiting first uploads

---
+++ index.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, resp) {
+ 
+    if (req.url === "/create") {
+        fs.readFile("MyPage.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+             
+            resp.end();
+        });
+    } else {
+    
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write('<h1>SK Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+        resp.end();
+    }
+});
+
+server.listen(3000);
+ 
+console.log('Server Started listening on 3000');
